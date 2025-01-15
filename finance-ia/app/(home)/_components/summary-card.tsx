2e6d97e9beabe6d85b658f61aa803c3c4e638368
eduardotecnologo <eduardotecnologo@hotmail.com>
2025-01-15 20:33:46 -0300

    fix: component summary card

---
+++ finance-ia/app/(home)/_components/summary-card.tsx
@@ -0,0 +1,24 @@
+import {Card,CardContent, CardHeader} from "@/app/_components/ui/card";
+import { ReactNode } from "react";
+
+interface SummaryCardProps {
+  icon: ReactNode;
+  title: string;
+  amount: number;
+}
+const SummaryCard = ({icon, title,amount}: SummaryCardProps) => {
+  return (
+    <Card>
+          <CardHeader>
+            {icon}
+            <p className="text-muted-foreground">{title}</p>
+          </ CardHeader>
+
+          <CardContent>
+            <p className="text-2xl font-bold">{amount}</p>
+          </CardContent>
+        </Card>
+  );
+}
+
+export default SummaryCard;
