63a8385a8c2a0b9d67ea1967be464e67da7ae5e4
eduardotecnologo <eduardotecnologo@hotmail.com>
2025-09-19 23:30:33 -0300

    feat: migrando o projeto para react

---
+++ ModeloGestao/day-trade-manager-react/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1>📈 Day Trade Manager</h1>
+        <p>Gerencie suas operações de day trade com eficiência</p>
+      </div>
+      {/* Aqui vamos importar os componentes */}
+      {/* <BrokerSelect /> */}
+      {/* <Dashboard /> */}
+      {/* ...outros componentes... */}
+    </div>
+  );
+}
+
+export default App;
